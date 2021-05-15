ba0242407376663611ba5a2ffeeeae4ee4720f58
Rosita Gonzales Mancilla <rositabgm@gmail.com>
2021-05-15 16:51:44 -0400

    fix get city

--- src/components/FiltrarPropiedad.jsx
+++ src/components/FiltrarPropiedad.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Propiedad from './Propiedad'
 import '../styles/listaPropiedades.css'
-const searchProp = ""
+
 const FiltrarPropiedad = props => {
+    const searchProp = "";
     return ( 
         <div className="lista-propiedades">
-            {
-                props.data.filter((element)=>{
-                    if(searchProp == ""){
-                        return element;
-                    }else if (element.data.city.toLowerCase().includes(searchProp.toLowerCase())){
-                        return element;
-                    }
-                }).map((element,key) => {
-                    return(
-                        <Propiedad data={element} key={key} />
-                    )
-                })
-            }
-
-            
+        {
+            props.data.filter((element)=>{
+                if(searchProp == ""){
+                    return element;
+                }else if (element.city.toLowerCase().includes(searchProp.toLowerCase())){
+                    return element;
+                }
+            }).map((element,key) => {
+                return(
+                    <Propiedad data={element} key={key} />
+                )
+            })
+        }
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import ListaPropiedades from './ListaPropiedades'
 import '../styles/home.css'
 import FiltrarPropiedad from './FiltrarPropiedad'
-
+import Propiedad from './Propiedad'
+import '../styles/listaPropiedades.css'
 
 const Home = (props) => {
     const [searchProp,setSearchProp] = useState("");
@@ -17,7 +18,23 @@ const Home = (props) => {
                 }} />
             </div>
             
-            <FiltrarPropiedad data={props.data}/>
+            <div className="lista-propiedades">
+            {
+                props.data.filter((element)=>{
+                    console.log(element.city);
+                    if(searchProp == ""){
+                        return element;
+                    }else if (element.city.toLowerCase().includes(searchProp.toLowerCase())){
+                        return element;
+                    }
+                }).map((element,key) => {
+                    return(
+                        <Propiedad data={element} key={key} />
+                    )
+                })
+            }
+            </div>
+
         </div>
     )
 }
